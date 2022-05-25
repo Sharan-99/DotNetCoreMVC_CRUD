ee4548055cb5a6dbe762f9a74981036fdfc4d41a
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 10:36:50 +0530

    first commit

---
+++ DotnetCoreMVC_CRUD/Models/ProductsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetCoreMVC_CRUD.Models
+{
+    public class ProductsDbContext:DbContext
+    {
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> options):base(options)
+        {
+
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
